6c92a5cd233a943b29f602092cd276c1571f1d34
Rob Collier <robcollier@gmail.com>
2022-12-13 00:05:38 +0000

    indenting and stuff

--- lua/robcollier/core/colorscheme.lua
+++ lua/robcollier/core/colorscheme.lua
@@ -1,10 +1,9 @@
-require('rose-pine').setup({
-    dark_variant = 'moon'
+require("rose-pine").setup({
+	dark_variant = "moon",
 })
-vim.cmd('colorscheme rose-pine')
-local status, _ = pcall(vim.cmd, 'colorscheme rose-pine')
-if not status then 
-    print('Colorscheme not installed')
-    return 
+vim.cmd("colorscheme rose-pine")
+local status, _ = pcall(vim.cmd, "colorscheme rose-pine")
+if not status then
+	print("Colorscheme not installed")
+	return
 end
-
--- lua/robcollier/core/keymaps.lua
+++ lua/robcollier/core/keymaps.lua
@@ -1,21 +1,22 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
-keymap.set('n', '<leader>nh', ':nohl<CR>')
-keymap.set('n', '<leader>ww', ':w<CR>') -- Save
-keymap.set('n', '<leader>wq', ':wq<CR>') -- Save & Quit
-keymap.set('n', '<leader>q', ':q!<CR>') -- Save & Quit
-keymap.set('n', 'x', '"_x') -- dont save small deletes to the register
+keymap.set("n", "<leader>;", ":")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader>ww", ":w<CR>") -- Save
+keymap.set("n", "<leader>wq", ":wq<CR>") -- Save & Quit
+keymap.set("n", "<leader>q", ":q!<CR>") -- Save & Quit
+keymap.set("n", "x", '"_x') -- dont save small deletes to the register
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 -- Telescope
-keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
-keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<CR>')
-keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<CR>')
-keymap.set('n', '<leader>bb', '<cmd>Telescope buffers<CR>')
-keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
-keymap.set('n', '<leader>gs', '<cmd>Telescope builtin.lsp_workspace_symbols<CR>')
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
+keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<CR>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+keymap.set("n", "<leader>gs", "<cmd>Telescope builtin.lsp_workspace_symbols<CR>")
 -- Keep search matches in the middle of the window
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
