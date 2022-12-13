938486ba8864c818421871c6452abee4e9b2dd4c
Rob Collier <robcollier@gmail.com>
2022-12-13 23:05:22 +0000

    WIP 🚀

--- lua/robcollier/core/keymaps.lua
+++ lua/robcollier/core/keymaps.lua
@@ -12,11 +12,17 @@ keymap.set("n", "x", '"_x') -- dont save small deletes to the register
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 -- Telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
 keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<CR>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
-keymap.set("n", "<leader>gs", "<cmd>Telescope builtin.lsp_workspace_symbols<CR>")
+
+keymap.set("n", "<leader>gs", "<cmd>Telescope lsp_document_symbols<CR>")
+keymap.set("n", "<leader>gf", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
+
+keymap.set("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<CR>")
+keymap.set("n", "<leader>sc", "<cmd>Telescope command_history<CR>")
+keymap.set("n", "<leader>sr", "<cmd>Telescope registers<CR>")
 -- Keep search matches in the middle of the window
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
@@ -24,3 +30,27 @@ keymap.set("n", "N", "Nzzzv")
 --keymap.set("n", "<leader>e", ":Lex 30<Cr>")
 -- Reload nvim config
 keymap.set("n", "<leader>r", "source $MYVIMRC<CR>")
+-- tab out of autopairs
+
+function EscapePair()
+	local closers = { ")", "]", "}", ">", "'", '"', "`", "," }
+	local line = vim.api.nvim_get_current_line()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local after = line:sub(col + 1, -1)
+	local closer_col = #after + 1
+	local closer_i = nil
+	for i, closer in ipairs(closers) do
+		local cur_index, _ = after:find(closer)
+		if cur_index and (cur_index < closer_col) then
+			closer_col = cur_index
+			closer_i = i
+		end
+	end
+	if closer_i then
+		vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
+	else
+		vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+	end
+end
+
+keymap.set("i", "<C-l>", "<cmd>lua EscapePair()<CR>", { noremap = true, silent = true })
--- lua/robcollier/plugins-setup.lua
+++ lua/robcollier/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -17,64 +17,64 @@ autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
 augroup end
 ]])
 
-local status, packer = pcall(require, 'packer')
+local status, packer = pcall(require, "packer")
 if not status then
-    return 
-end 
+	return
+end
 
-return packer.startup(function (use)
-    use 'wbthomason/packer.nvim'
-    -- other plugins here
-    use 'rose-pine/neovim' 
-    use 'tpope/vim-surround'
-    use 'vim-scripts/ReplaceWithRegister' 
-    use 'nvim-lua/plenary.nvim'
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-    use {'nvim-telescope/telescope.nvim'}
-    -- Completion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    -- snippets
-    use("L3MON4D3/LuaSnip") -- snippet engine
-    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-    use("rafamadriz/friendly-snippets") -- useful snippets
-    -- LSP
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use {'glepnir/lspsaga.nvim', branch = 'main'}
-    use 'onsails/lspkind.nvim'
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
-    -- vs-code like icons
-    use("nvim-tree/nvim-web-devicons")
-    -- formatting & linting
-    use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-    use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+return packer.startup(function(use)
+	use("wbthomason/packer.nvim")
+	-- other plugins here
+	use("rose-pine/neovim")
+	use("tpope/vim-surround")
+	use("vim-scripts/ReplaceWithRegister")
+	use("nvim-lua/plenary.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim" })
+	-- Completion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
+	-- LSP
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("onsails/lspkind.nvim")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-    -- treesitter configuration
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end,
-    })
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-    -- auto closing
-    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-    -- git integration
-    use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
-    -- end plugins
-    if packer_bootstrap then 
-        require('packer').sync()
-    end
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	-- end plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
